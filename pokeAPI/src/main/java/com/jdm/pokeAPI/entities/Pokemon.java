f85cc079efa50b85fe8ba6ca81e7cd885f7333a2
Joe <56753837+Kizmit@users.noreply.github.com>
2021-08-28 16:16:10 -0500

    package skeleton, home page with background image

---
+++ pokeAPI/src/main/java/com/jdm/pokeAPI/entities/Pokemon.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jdm.pokeAPI.entities;
+
+/**
+ *
+ * @author Joe McAdams
+ * @email joedmcadams@gmail.com
+ * 
+ */
+public class Pokemon {
+
+}
