dee8cd0b20c46c73556a0465bb78e2e9971405c8
Joe <56753837+Kizmit@users.noreply.github.com>
2021-08-28 15:13:28 -0500

    Spring Intializer initial commit

---
+++ pokeAPI/src/main/java/com/jdm/pokeAPI/PokeApiApplication.java
@@ -0,0 +1,13 @@
+package com.jdm.pokeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiApplication.class, args);
+	}
+
+}
